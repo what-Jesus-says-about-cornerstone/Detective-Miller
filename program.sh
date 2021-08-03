01f546d2ff08ae21106955f1f4beb3f94483a8ce
canterbury <canterbury@program>
2021-08-03 18:40:03 +0300

    to program.sh i add main

--- program.sh
+++ program.sh
@@ -10,4 +10,12 @@ repl(){
     :nrepl? false
 }
 
+
+main(){
+  clojure \
+    -J-Dclojure.core.async.pool-size=1 \
+    -J-Dclojure.compiler.direct-linking=false \
+    -M -m canterbury.main
+}
+
 "$@"
