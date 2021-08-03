e608c59bbe9ea8757363e8ad13eadbaff57ec96f
canterbury <canterbury@libraries>
2021-08-03 20:34:40 +0300

    we revert "i use swing library and show jframe window"
    This reverts commit 6a5555df8c4fb755a10c7d1a3785493afcb0c9c3.

--- src/canterbury/main.clj
+++ src/canterbury/main.clj
@@ -7,32 +7,19 @@
                                      pipeline pipeline-async]]
    [clojure.string]
    [clojure.spec.alpha :as s]
-   [clojure.java.io :as io])
-  (:import
-   (javax.swing JFrame JLabel JButton SwingConstants JMenuBar JMenu JTextArea)
-   (java.awt Canvas Graphics)
-   (java.awt.event WindowListener KeyListener KeyEvent)))
+   [clojure.java.io :as io]))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
 (clojure.spec.alpha/check-asserts true)
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
 (defonce stateA (atom nil))
-(defonce ^JFrame jframe nil)
 
 (defn -main [& args]
   (println ::-main)
   (let [data-dir (-> (io/file (System/getProperty "user.dir")) (.getCanonicalPath))]
     (reset! stateA {})
     (add-watch stateA :watch-fn (fn [k stateA old-state new-state] new-state))
-
-    (let [jframe (JFrame. "canterbury")]
-      (doto jframe
-        (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-        (.setSize 1600 1200)
-        (.setLocationByPlatform true)
-        (.setVisible true))
-      (alter-var-root #'canterbury.main/jframe (constantly jframe)))
 
     (go)))
 
