25dad454c08b26891f2a0b4a96dc718c17b542f2
canterbury <canterbury@program>
2021-08-03 18:39:44 +0300

    i am using deps-repl library

--- program.sh
+++ program.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-
+repl(){
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns canterbury.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
+}
 
 "$@"
