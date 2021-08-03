e4ac68bb614d289b676986fd1fef37412620742e
canterbury <canterbury@libraries>
2021-08-03 20:34:48 +0300

    we revert "to program.sh i add j-package"
    This reverts commit 91e77409c4faa90b9c8b1486af4c70e580cdb413.

--- program.sh
+++ program.sh
@@ -29,31 +29,4 @@ uberjar(){
   mv out/canterbury.standalone.jar out/jpackage-input/
 }
 
-j-package(){
-  OS=${1:?"Need OS type (windows/linux/mac)"}
-
-  echo "Starting compilation..."
-
-  if [ "$OS" == "windows" ]; then
-    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
-          
-  elif [ "$OS" == "linux" ]; then
-      J_ARG="--linux-shortcut"
-  else
-      J_ARG=""
-  fi
-
-  jpackage \
-    --input out/jpackage-input \
-    --dest out \
-    --main-jar canterbury.standalone.jar \
-    --name "canterbury" \
-    --main-class clojure.main \
-    --arguments -m \
-    --arguments canterbury.main \
-    --app-version "1" \
-    $J_ARG
-}
-
-
 "$@"
